75b4a92cebd6b7a5e64563624d0819482512a8b5
Viola Kempa <viola.kempa@code.berlin>
2023-03-11 17:39:31 +0100

    fixed icons

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-to_do_list = {
-    '1' : {'task': 'Complete project proposal', 'status': 'in progress'},
-    '2' : {'task': 'Attend meeting with team', 'status': 'complete'},
-    '3' : {'task': 'Research competitors', 'status': 'not started'},
-    '4' : {'task': 'Create wireframe designs', 'status': 'in progress'},
-    '5' : {'task': 'Review feedback from stakeholders', 'status': 'not started'},
-    '6' : {'task': 'Finalize project plan', 'status': 'complete'}
-}
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,9 +22,17 @@ def general():
 def user_profile(username):
     return render_template('user.html', username=username)
 
-@app.route('/todo')
-def to_do():
-    return render_template('todo.html', to_do_list=to_do_list)
+@app.route('/todo/<slug>')
+def to_do(slug): #for loop
+    to_do_list = {
+    '1' : {'task': 'Complete project proposal', 'status': 'in progress'},
+    '2' : {'task': 'Attend meeting with team', 'status': 'complete'},
+    '3' : {'task': 'Research competitors', 'status': 'not started'},
+    '4' : {'task': 'Create wireframe designs', 'status': 'in progress'},
+    '5' : {'task': 'Review feedback from stakeholders', 'status': 'not started'},
+    '6' : {'task': 'Finalize project plan', 'status': 'complete'}
+}
+    return render_template('todo.html', to_do=to_do_list)
 
 if __name__ == '__main__':
     app.run()
