dd74c3f0048784f3f7e1ba231ebbc6a8fba598a5
Viola Kempa <viola.kempa@code.berlin>
2023-03-13 19:57:34 +0100

    templating, background

--- app.py
+++ app.py
@@ -6,24 +6,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/studies')
 def about():
     return render_template('studies.html')
+
 
 @app.route('/private')
 def contact():
     return render_template('private.html')
 
+
 @app.route("/general")
 def general():
     return render_template("general.html")
+
 
 @app.route('/user/<username>')
 def user_profile(username):
     return render_template('user.html', username=username)
 
+
 @app.route('/todo/<slug>')
-def to_do(slug): #for loop
+def to_do(slug):
     to_do_list = {
     '1' : {'task': 'Complete project proposal', 'status': 'in progress'},
     '2' : {'task': 'Attend meeting with team', 'status': 'complete'},
